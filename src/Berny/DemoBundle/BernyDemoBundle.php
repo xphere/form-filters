03ce36f818aa9ae9f3adf304d6b5db610ce5c3f6
Berny Cantos <be@rny.cc>
2013-05-15 21:12:31 +0200

    add Berny\DemoBundle

---
+++ src/Berny/DemoBundle/BernyDemoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Berny\DemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BernyDemoBundle extends Bundle
+{
+}
---
+++ src/Berny/DemoBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Berny\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array(
+            'list' => array(
+            ),
+        );
+    }
+}
