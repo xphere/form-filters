202951dd00bb1b0cd6c02f862608a3819c5b0c11
Berny Cantos <be@rny.cc>
2013-05-29 00:10:18 +0200

    clean

--- src/Berny/DemoBundle/Form/Type/CriterionType.php
+++ src/Berny/DemoBundle/Form/Type/CriterionType.php
@@ -23,7 +23,6 @@ class CriterionType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildForm($builder, $options);
         $builder
             ->add('field', 'choice', array(
                 'label' => false,
